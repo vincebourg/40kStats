0f879aa5bc958949acae911ebc4c3cf07c646a38
Vince <vincebourgeois@gmail.com>
2021-01-30 21:01:34 +0100

    moved Dice

---
+++ 40Stats.Core/Dice/Dice.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace _40Stats.Core.Dices
+{
+    public class Dice
+    {
+        public int Roll() => new Random().Next(1, 6);
+    }
+}
--- 40kStats.Test/Dice.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace _40kStats.Test
-{
-    internal class Dice
-    {
-        internal int Roll() => new Random().Next(1, 6);
-    }
-}
