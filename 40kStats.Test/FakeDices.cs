884f993f1c2473ed484eb67198cc4f261fd44f2f
Vince <vincebourgeois@gmail.com>
2021-04-04 19:21:14 +0200

    refactored fake dices

---
+++ 40kStats.Test/FakeDices.cs
@@ -0,0 +1,53 @@
+﻿using _40Stats.Core.Dices;
+using System;
+
+namespace _40kStats.Test
+{
+    
+    internal class OnlyRollOne : IRoll
+    {
+        public int Roll()
+        {
+            return 1;
+        }
+    }
+
+    internal class OnlyRollTwo : IRoll
+    {
+        public int Roll()
+        {
+            return 2;
+        }
+    }
+
+    internal class OnlyRollThree : IRoll
+    {
+        public int Roll()
+        {
+            return 3;
+        }
+    }
+    internal class OnlyRollFour : IRoll
+    {
+        public int Roll()
+        {
+            return 4;
+        }
+    }
+
+    internal class OnlyRollFive : IRoll
+    {
+        public int Roll()
+        {
+            return 5;
+        }
+    }
+
+    internal class OnlyRollSix : IRoll
+    {
+        public int Roll()
+        {
+            return 6;
+        }
+    }
+}
