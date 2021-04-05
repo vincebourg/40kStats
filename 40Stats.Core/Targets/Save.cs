df7322e006853d7da722dc5285b6be46335d25dc
Vince <vincebourgeois@gmail.com>
2021-04-05 17:58:19 +0200

    added invulnerable saves

--- 40Stats.Core/Targets/Save.cs
+++ 40Stats.Core/Targets/Save.cs
@@ -3,11 +3,11 @@ using _40Stats.Core.Rolls;
 
 namespace _40Stats.Core.Targets
 {
-    public record Save(int Expected)
+    public record Save(int Expected, bool IsInvulnerable = false)
     {
         public SaveRoll Roll(IRoll dice, int ArmorPenetration = 0)
         {
-            return new(Expected + ArmorPenetration, dice.Roll());
+            return new(Expected + (IsInvulnerable ? 0 : ArmorPenetration), dice.Roll());
         }
     };
 
--- 40kStats.Test/SaveTests.cs
+++ 40kStats.Test/SaveTests.cs
@@ -20,5 +20,22 @@ namespace _40kStats.Test
             SaveRoll result = save.Roll(new Dice());
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void save_can_be_invulnerable()
+        {
+            Save save = new(4, true);
+            SaveRoll result = save.Roll(new OnlyRollFour());
+            Assert.IsTrue(result.Saved);
+        }
+
+        [TestMethod]
+        public void invulnerable_save_are_not_afected_by_Armor_penetration()
+        {
+            var expectedSaveRoll = 4;
+            Save save = new(expectedSaveRoll, true);
+            SaveRoll result = save.Roll(new OnlyRollFour(), 4);
+            Assert.AreEqual(expectedSaveRoll, result.Expected);
+        }
     }
 }
