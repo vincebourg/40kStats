1aff283996cf18b51dcf6dc47b0a4b205719f2bc
Vincent Bourgeois <vincent.bourgeois@codeworks.fr>
2025-04-02 20:31:53 +0200

    added json profile loading

---
+++ profileLoaderTests/Profiles.cs
@@ -0,0 +1,7 @@
+﻿namespace profileLoaderTests;
+
+public record Profiles(WeaponProfile[] WeaponProfiles, TargetProfile[] TargetProfiles);
+
+public record TargetProfile(string Name, int Toughness, int Save, int Wounds);
+
+public record WeaponProfile(string Name, string Attacks, int WeaponSkill, int Strenght, int ArmorPenetration, int Damage);
---
+++ profileLoaderTests/loadJson.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+
+namespace profileLoaderTests;
+
+[TestClass]
+public sealed class loadJson
+{
+    const string profileJson = """
+            {
+                "weaponProfiles":[
+                {
+                        "name": "pulse blaster",
+                        "attacks": "2",
+                        "weaponSkill": 3,
+                        "strength": 6,
+                        "armorPenetration": 1,
+                        "damage": 1
+                }],
+                "targetProfiles": [
+                    {
+                        "name": "space marine",
+                        "toughness": 4,
+                        "save": 3,
+                        "wounds": 2
+                    }]
+            }
+            """;
+
+    [TestMethod]
+    public void Can_extract_profile_from_json()
+    {
+        var result = JsonConvert.DeserializeObject<Profiles>(profileJson);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.WeaponProfiles.Length);
+        Assert.AreEqual(1, result.TargetProfiles.Length);
+    }
+
+    [TestMethod]
+    public void Can_extract_Target_dictionary()
+    {
+
+    }
+}
